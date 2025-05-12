4f0bc1a2a7cf178e57f6d26c24798f368aedd872
paulaCorrea969 <pcorrea66315@universidadean.edu.co>
2025-05-12 09:30:34 -0500

    puzzles.py

--- puzzles.py
+++ puzzles.py
@@ -1,13 +1,11 @@
 import random
 
-# Diccionario base para diferentes visualizaciones
 VALORES = {
     "numeros": ["1", "2", "3", "4"],
     "letras": ["A", "B", "C", "D"],
     "simbolos": ["", "", "", ""]
 }
 
-# Tablero base completo
 TABLEROS_COMPLETOS = [
     [
         ["1", "2", "3", "4"],
@@ -27,11 +25,9 @@ def obtener_tablero(dificultad, tipo_visualizacion):
     tablero_base = random.choice(TABLEROS_COMPLETOS)
     valores = VALORES[tipo_visualizacion]
     
-    # Convertir al tipo seleccionado
     conversion = dict(zip(["1", "2", "3", "4"], valores))
     tablero = [[conversion[val] for val in fila] for fila in tablero_base]
 
-    # Elegir cuántas casillas vaciar
     vacias = 6 if dificultad == "facil" else 10
     posiciones = [(i, j) for i in range(4) for j in range(4)]
     random.shuffle(posiciones)
